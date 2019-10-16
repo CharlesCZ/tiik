11e4c5840821eee57f9feb5a1a0d0b2afa9f9298
jan hp-g6.local <jan@hp-g6.local>
2019-10-16 11:37:04 +0200

    add countChars method

--- tiik/src/main/java/Main.java
+++ tiik/src/main/java/Main.java
@@ -1,18 +1,32 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 
 public class Main {
 
+    public static HashMap<Character,Integer> charsMap;
     public static void main(String[] args) throws IOException {
+
+        //initiate
         String projectPath = System.getProperty("user.dir");
+        charsMap = new HashMap<Character,Integer> ();
         // pass the path to the file as a parameter
         FileReader fr =
                 new FileReader(projectPath + "\\src\\main\\resources\\test.txt");
 
         int i;
-        while ((i=fr.read()) != -1)
-            System.out.print((char) i);
-    }
+        while ((i=fr.read()) != -1){
+            countChars((char)i);
+        System.out.print("\n");
+        System.out.print(charsMap);
+        }
+}
 
+    public static void countChars(char inputChar){
+        if (charsMap.containsKey(inputChar)) {
+            charsMap.replace(inputChar, charsMap.get(inputChar)+1);
+        }else
+            charsMap.putIfAbsent(inputChar,1);
     }
+}
