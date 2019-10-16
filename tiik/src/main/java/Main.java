afa2b0d5ce5b5d462a8ade62b9340923571f0b64
CharlesCZ <karakankaczyn@gmail.com>
2019-10-16 10:50:32 +0200

    added file input

--- tiik/src/main/java/Main.java
+++ tiik/src/main/java/Main.java
@@ -1,8 +1,18 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("hello");
+        // pass the path to the file as a parameter
+        FileReader fr =
+                new FileReader("C:\\Users\\czarek\\gitrepos\\tiik\\src\\main\\resources\\test.txt");
+
+        int i;
+        while ((i=fr.read()) != -1)
+            System.out.print((char) i);
     }
 
     }
