9022e73b4c15776344c86c89ee101db3024922ba
CharlesCZ <karakankaczyn@gmail.com>
2019-10-22 14:12:25 +0200

    added unitOfInformation and probability

--- tiik/src/main/java/Main.java
+++ tiik/src/main/java/Main.java
@@ -1,26 +1,44 @@
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Main {
 
     public static HashMap<Character,Integer> charsMap;
+public static List<Sign> signs=new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
 
         //initiate
         String projectPath = System.getProperty("user.dir");
-        charsMap = new HashMap<Character,Integer> ();
+        charsMap = new HashMap<> ();
         // pass the path to the file as a parameter
         FileReader fr =
                 new FileReader(projectPath + "\\src\\main\\resources\\test.txt");
 
-        int i;
+        int i,quantity=0;
         while ((i=fr.read()) != -1){
             countChars((char)i);
-        System.out.print("\n");
-        System.out.print(charsMap);
+            ++quantity;
         }
+        System.out.println(charsMap);
+
+        //quantity of input chracters
+        final int sizeOfChars=quantity;
+
+      charsMap.forEach((character, integer) -> {
+          Sign sign=new Sign();
+          sign.setCharacter(character);
+          sign.setOccurences(integer);
+          sign.setProbability((double)integer/sizeOfChars);
+          sign.setUnitOfInformation(unitOfInformation(sign.getProbability()));
+          signs.add(sign);
+      });
+
+
+      signs.forEach(System.out::println);
 }
 
     public static void countChars(char inputChar){
@@ -28,5 +46,12 @@ public class Main {
             charsMap.replace(inputChar, charsMap.get(inputChar)+1);
         }else
             charsMap.putIfAbsent(inputChar,1);
+    }
+
+    public static double unitOfInformation(Double probabilityOfUnitOfInformation){
+
+        return Math.log(1/probabilityOfUnitOfInformation)/ Math.log(2);
+
+
     }
 }
---
+++ tiik/src/main/java/Sign.java
@@ -0,0 +1,54 @@
+public class Sign {
+
+    Character character;
+
+    Double probability;
+
+    Double unitOfInformation;
+
+    Integer occurences;
+
+    public Character getCharacter() {
+        return character;
+    }
+
+    public void setCharacter(Character character) {
+        this.character = character;
+    }
+
+    public Double getProbability() {
+        return probability;
+    }
+
+    public void setProbability(Double probability) {
+        this.probability = probability;
+    }
+
+    public Double getUnitOfInformation() {
+        return unitOfInformation;
+    }
+
+    public void setUnitOfInformation(Double unitOfInformation) {
+        this.unitOfInformation = unitOfInformation;
+    }
+
+    public Integer getOccurences() {
+        return occurences;
+    }
+
+    public void setOccurences(Integer occurences) {
+        this.occurences = occurences;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Sign{" +
+                "character=" + character +
+                ", probability=" + probability +
+                ", unitOfInformation=" + unitOfInformation +
+                ", occurences=" + occurences +
+                '}';
+    }
+}
