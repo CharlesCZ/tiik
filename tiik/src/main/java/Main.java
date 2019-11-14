a8ad704616c20934f62b3a439c3e03462c90f17f
CharlesCZ <karakankaczyn@gmail.com>
2019-11-14 17:04:46 +0100

    refactorign

--- tiik/src/main/java/Main.java
+++ tiik/src/main/java/Main.java
@@ -6,52 +6,26 @@ import java.util.List;
 
 public class Main {
 
-    public static HashMap<Character,Integer> charsMap;
-public static List<Sign> signs=new ArrayList<>();
+
+private static final SignService signService=new SignService();
 
     public static void main(String[] args) throws IOException {
 
         //initiate
         String projectPath = System.getProperty("user.dir");
-        charsMap = new HashMap<> ();
+
         // pass the path to the file as a parameter
         FileReader fr =
                 new FileReader(projectPath + "\\src\\main\\resources\\test.txt");
 
-        int i,quantity=0;
-        while ((i=fr.read()) != -1){
-            countChars((char)i);
-            ++quantity;
-        }
-        System.out.println(charsMap);
+     int quantity= signService.countChars(fr);
 
+signService.setCharStatistics(quantity);
         //quantity of input chracters
-        final int sizeOfChars=quantity;
-
-      charsMap.forEach((character, integer) -> {
-          Sign sign=new Sign();
-          sign.setCharacter(character);
-          sign.setOccurences(integer);
-          sign.setProbability((double)integer/sizeOfChars);
-          sign.setUnitOfInformation(unitOfInformation(sign.getProbability()));
-          signs.add(sign);
-      });
 
 
-      signs.forEach(System.out::println);
+
 }
 
-    public static void countChars(char inputChar){
-        if (charsMap.containsKey(inputChar)) {
-            charsMap.replace(inputChar, charsMap.get(inputChar)+1);
-        }else
-            charsMap.putIfAbsent(inputChar,1);
-    }
 
-    public static double unitOfInformation(Double probabilityOfUnitOfInformation){
-
-        return Math.log(1/probabilityOfUnitOfInformation)/ Math.log(2);
-
-
-    }
 }
---
+++ tiik/src/main/java/SignService.java
@@ -0,0 +1,69 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class SignService {
+
+    private   HashMap<Character,Integer> charsMap = new HashMap<> ();
+    private List<Sign> signs=new ArrayList<>();
+
+
+    private   void countChar(char inputChar){
+        if (charsMap.containsKey(inputChar)) {
+            charsMap.replace(inputChar, charsMap.get(inputChar)+1);
+        }else
+            charsMap.putIfAbsent(inputChar,1);
+    }
+    public  int countChars( FileReader fr ) throws IOException {
+        int i,quantity=0;
+        while ((i=fr.read()) != -1){
+            countChar((char)i);
+            ++quantity;
+        }
+
+        return quantity;
+    }
+    public  double unitOfInformation(Double probabilityOfUnitOfInformation){
+
+        return Math.log(1/probabilityOfUnitOfInformation)/ Math.log(2);
+
+
+    }
+
+
+    public void setCharStatistics( final int sizeOfChars){
+
+
+        charsMap.forEach((character, integer) -> {
+            Sign sign=new Sign();
+            sign.setCharacter(character);
+            sign.setOccurences(integer);
+            sign.setProbability((double)integer/sizeOfChars);
+            sign.setUnitOfInformation(unitOfInformation(sign.getProbability()));
+            signs.add(sign);
+        });
+
+
+        signs.forEach(System.out::println);
+
+    }
+
+
+    public HashMap<Character, Integer> getCharsMap() {
+        return charsMap;
+    }
+
+    public void setCharsMap(HashMap<Character, Integer> charsMap) {
+        this.charsMap = charsMap;
+    }
+
+    public List<Sign> getSigns() {
+        return signs;
+    }
+
+    public void setSigns(List<Sign> signs) {
+        this.signs = signs;
+    }
+}
